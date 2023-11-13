d6411b347835dacbd057040c3bd3de1975bd5dc6
GreaterDogFr <benjaminherve76120@outlook.fr>
2023-11-13 11:30:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            product: 'Chaussettes',
+            image : './assets/images/socks_blue.jpg',
+            url : '/chaussette'
+        }
+    }
+})
