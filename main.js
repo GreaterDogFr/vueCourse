4ac7e7cb4c7e3250f1905d7e13f4a7cee63e8688
GreaterDogFr <benjaminherve76120@outlook.fr>
2023-11-13 13:55:13 +0100

    part5 done

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const app = Vue.createApp({
     data() {
-        return{
+        return {
+            cart: 0,
             product: 'Chaussettes',
-            image : './assets/images/socks_blue.jpg',
-            url : '/chaussette'
+            url: '/chaussette',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% coton', '30% laine', '20% polyester'],
+            variants: [
+                { id: 1234, color: 'bleu', image: './assets/images/socks_blue.jpg' },
+                { id: 3245, color: 'vert', image: './assets/images/socks_green.jpg' }
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        changeImg(variantImage) {
+            this.image = variantImage
         }
     }
 })
